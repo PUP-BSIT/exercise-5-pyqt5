952596c7b37d803c9b44da748cc54a1b68688ce8
gutierrez-kingandrei <kingandreibgutierrez@iskolarngbayan.pup.edu.ph>
2025-03-19 16:45:21 +0800

    Add question 5 and logic

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ user_score = 0 # Initialize a variable for score count
 
 
 
-# TODO(Cordova): Add your question, and implement answer checking logic
+
 
 
 
@@ -50,16 +50,17 @@ user_score = 0 # Initialize a variable for score count
 
 
 # TODO(Gutierrez): Add your question, and implement answer checking logic
+print("\n(King Andrei Gutierrez) When is my birthday?")
+print("a)February 21, 2004          c)February 20, 2004")
+print("b)February 20, 2005          d)February 21, 2005")
 
+user_answer = input("Enter your answer: ").upper()
 
-
-
-
-
-
-
-
-
-
+if user_answer == 'D':
+    print(f"{user_answer} is Correct!")
+    user_score += 1
+else:
+    print(f"{user_answer} is incorrect!. The correct answer is D.")
+    
 # Display user score
 print(f"\nCongratulations! You got {user_score} out of {total_items} items.")
