41cdabd53ee3d6a72ca309963479cd854105e084
cordova-aronstephen <aronstephenscordova@gmail.com>
2025-03-19 16:39:06 +0800

    Declare necessary variables, and add tasks

---
+++ main.py
@@ -0,0 +1,65 @@
+total_items = 5 # Initialize a variable with the quiz item count.
+user_score = 0 # Initialize a variable for score count
+# user_answer variable will be use to store user's answer
+# TODO(Arguelles): Add your question, and implement answer checking logic
+
+
+
+
+
+
+
+
+
+
+
+# TODO(Caya): Add your question, and implement answer checking logic
+
+
+
+
+
+
+
+
+
+
+
+# TODO(Condino): Add your question, and implement answer checking logic
+
+
+
+
+
+
+
+
+
+
+
+# TODO(Cordova): Add your question, and implement answer checking logic
+
+
+
+
+
+
+
+
+
+
+
+# TODO(Gutierrez): Add your question, and implement answer checking logic
+
+
+
+
+
+
+
+
+
+
+
+# Display user score
+print(f"\nCongratulations! You got {user_score} out of {total_items} items.")
